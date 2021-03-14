af9807a35774e32bdeda72060cd31cb9fa9a38fb
Yabkoy <yabuszko05@gmail.com>
2021-03-14 22:20:26 +0100

    add drawing surface of cube

--- RubicCube/include/SFMLVisualization.h
+++ RubicCube/include/SFMLVisualization.h
@@ -6,11 +6,17 @@ namespace sfmlGrap
 	class surfaceShape : public sf::Drawable
 	{
 	private:
-		sf::RectangleShape testRS;
+		std::array<sf::RectangleShape, 9> block;
+		
+		sf::Vector2f entryPointPosition;
 
 		virtual void draw(sf::RenderTarget& mainTarget, sf::RenderStates states) const;
+
 	public:
-		surfaceShape();
+		surfaceShape(const sf::Vector2f& entryPointPosition = sf::Vector2f(0, 0));
+
+		void initSurface();
+		void setEntryPointPosition(const sf::Vector2f& epp);
 	};
 
 
@@ -19,7 +25,8 @@ namespace sfmlGrap
 		private:
 			sf::RenderWindow* RW;
 			sf::Event mainEvent;
-			surfaceShape testSH;
+
+			surfaceShape testShape;
 
 			void mainUpdateLoop();
 		public:
--- RubicCube/src/SFMLVisualization.cpp
+++ RubicCube/src/SFMLVisualization.cpp
@@ -2,19 +2,60 @@
 
 void sfmlGrap::surfaceShape::draw(sf::RenderTarget& mainTarget, sf::RenderStates states) const
 {
-	mainTarget.draw(testRS);
+	for (int i = 0; i < block.size(); i++)
+		mainTarget.draw(block[i]);
 }
 
-sfmlGrap::surfaceShape::surfaceShape()
+sfmlGrap::surfaceShape::surfaceShape(const sf::Vector2f& entryPointPosition)
 {
-	testRS.setSize(sf::Vector2f(100, 100));
-	testRS.setFillColor(sf::Color::Red);
+
+}
+
+void sfmlGrap::surfaceShape::initSurface()
+{
+	const int elementPadding = 60;
+
+	sf::Vector2f allPositionsOfBlocks[9] =
+	{
+		sf::Vector2f(entryPointPosition.x - elementPadding, entryPointPosition.y - elementPadding),
+		sf::Vector2f(entryPointPosition.x, entryPointPosition.y - elementPadding),
+		sf::Vector2f(entryPointPosition.x + elementPadding, entryPointPosition.y - elementPadding),
+
+		sf::Vector2f(entryPointPosition.x - elementPadding, entryPointPosition.y),
+		sf::Vector2f(entryPointPosition.x, entryPointPosition.y),
+		sf::Vector2f(entryPointPosition.x + elementPadding, entryPointPosition.y),
+
+		sf::Vector2f(entryPointPosition.x - elementPadding, entryPointPosition.y + elementPadding),
+		sf::Vector2f(entryPointPosition.x, entryPointPosition.y + elementPadding),
+		sf::Vector2f(entryPointPosition.x + elementPadding, entryPointPosition.y + elementPadding),
+
+		
+	};
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			static int elementDone = 0;
+			block[elementDone].setFillColor(sf::Color::White);
+			block[elementDone].setSize(sf::Vector2f(50, 50));
+			block[elementDone].setPosition(allPositionsOfBlocks[elementDone]);
+			elementDone++;
+		}
+	}
+}
+void sfmlGrap::surfaceShape::setEntryPointPosition(const sf::Vector2f& epp)
+{
+	entryPointPosition = epp;
 }
 
 
 sfmlGrap::mainSFMLVis::mainSFMLVis()
 {
 	RW = new sf::RenderWindow(sf::VideoMode(WINDOW_W, WINDOW_H, 32), "Rubic Cube");
+
+	testShape.setEntryPointPosition(sf::Vector2f(200, 200));
+	testShape.initSurface();
 
 	mainUpdateLoop();
 }
@@ -35,7 +76,7 @@ void sfmlGrap::mainSFMLVis::mainUpdateLoop()
 
 		RW->clear(sf::Color::Black);
 
-		RW->draw(testSH);
+		RW->draw(testShape);
 
 		RW->display();
 	}
